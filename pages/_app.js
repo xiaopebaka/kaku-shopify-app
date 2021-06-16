f15ff4795c498db5dc6bc840465bef21dbe3c02e
maigo <baka9@me.com>
2021-06-16 18:05:13 +0900

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,88 @@
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import App from "next/app";
+import { AppProvider } from "@shopify/polaris";
+import { Provider, useAppBridge } from "@shopify/app-bridge-react";
+import { authenticatedFetch } from "@shopify/app-bridge-utils";
+import { Redirect } from "@shopify/app-bridge/actions";
+import "@shopify/polaris/dist/styles.css";
+import translations from "@shopify/polaris/locales/en.json";
+import React from 'react';
+import Head from 'next/head';
+import ClientRouter from '../components/ClientRouter';
+
+
+function userLoggedInFetch(app) {
+  const fetchFunction = authenticatedFetch(app);
+
+  return async (uri, options) => {
+    const response = await fetchFunction(uri, options);
+
+    if (
+      response.headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1"
+    ) {
+      const authUrlHeader = response.headers.get(
+        "X-Shopify-API-Request-Failure-Reauthorize-Url"
+      );
+
+      const redirect = Redirect.create(app);
+      redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/auth`);
+      return null;
+    }
+
+    return response;
+  };
+}
+
+function MyProvider(props) {
+  const app = useAppBridge();
+
+  const client = new ApolloClient({
+    fetch: userLoggedInFetch(app),
+    fetchOptions: {
+      credentials: "include",
+    },
+  });
+
+  const Component = props.Component;
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...props} />
+    </ApolloProvider>
+  );
+}
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, shopOrigin } = this.props;
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <AppProvider i18n={translations}>
+          <Provider
+            config={{
+              apiKey: API_KEY,
+              shopOrigin: shopOrigin,
+              forceRedirect: true,
+            }}
+          >
+            <ClientRouter />
+            <MyProvider Component={Component} {...pageProps} />
+          </Provider>
+        </AppProvider>
+      </React.Fragment>
+    );
+  }
+}
+
+MyApp.getInitialProps = async ({ ctx }) => {
+  return {
+    shopOrigin: ctx.query.shop,
+  };
+};
+
+export default MyApp;
